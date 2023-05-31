a1ea717d1b190fb2830f25f024339417dc62b978
chrascher <cgs.schaefer@gmail.com>
2023-05-31 11:49:50 +0200

    add countend and time montitoring zu client proxy

--- src/main/java/at/cgsit/jeemicro/proxy/CreateInfoProxy.java
+++ src/main/java/at/cgsit/jeemicro/proxy/CreateInfoProxy.java
@@ -5,6 +5,9 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/createinfo")
@@ -18,6 +21,8 @@ public interface CreateInfoProxy {
     @GET
     @Path("/ext")
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(name = "ciproxy_ext_counter", description = "How many primality checks have been performed.")
+    @Timed(name = "ciproxy_ext_timer", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
     SimpleDTO createNumber2();
 
 }
