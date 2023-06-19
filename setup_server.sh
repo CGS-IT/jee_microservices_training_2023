bfacc2f798061d01edf78185fe938c591e08fd80
ext_christian.schaef <christian.schaefer@cgs.at>
2023-06-19 18:46:21 +0200

    permssion

--- setup_server.sh
+++ setup_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo -n "creating database via sql script"
+
 export PGPASSWORD=postgres
+
 psql -U postgres -d postgres -a -f ./jee_microservice_start/src/main/database_scripts/create_db.sql
+
+
